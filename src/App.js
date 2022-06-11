2a433a2d53809658fe84c2b2efa9defceed075ff
Apoorva <ojha.apoorva98@gmail.com>
2022-06-11 20:30:11 +0530

    File and Folder Structure Defined

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import ExerciseDetail from './components/pages/ExerciseDetail';
+import Home from './components/pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/exercise/:id" element={ <ExerciseDetail /> } />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
